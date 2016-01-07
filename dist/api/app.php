29f95b09d16b07bda8d10ebadfd64313fe6ef8be
lduboeuf <lionel.duboeuf@gmail.com>
2016-01-07 10:17:37 +0100

    Initial dist subtree commit

---
+++ dist/api/app.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$app = require __DIR__.'/bootstrap.php';
+$app['debug'] = true;
+
+
+//Allow PHP's built-in server to serve our static content in local dev:
+$filename = dirname(__DIR__).preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+$app['monolog']->addInfo("kikou file:".$filename);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+$app->get('/', function () use ($app) {
+ 	return $app->sendFile(dirname(__DIR__).'/index.html');
+ });
+
+
+
+$app['monolog']->addInfo("server loaded");
+
+$app->run();
---
+++ dist/api/db/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE main.todos (
+  id INTEGER PRIMARY KEY  NOT NULL ,
+  name VARCHAR NOT NULL );
+
+INSERT INTO todos VALUES (1,"sleep");
+INSERT INTO todos VALUES (2,"take fresh air");
